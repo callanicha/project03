f1956a769e034f13cb9cfe07010902fc51e2f5ef
natthanicha <natthanicha.vongjarit@epita.fr>
2024-10-14 16:03:36 +0200

    some commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const usersRouter = require("./routes/users.js")
+
+app.use("/api/", usersRouter)
+
+app.use(express.json());
+
+//GET METHODS
+app.get("/", (req, res) => {
+    res.json({
+        msg: "This the message"
+    })
+})
+
+
+app.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}`);
+});
+
+
+//Open your browser and go to http://localhost:3000. 
+// You should see the message "Welcome to our simple REST API!"
+
---
+++ routes/users.js
@@ -0,0 +1,71 @@
+const express = require("express")
+const router = express.Router()
+
+const usersArray = [
+	{
+	"ID": 1,
+	"firstName": "John",
+	"lastName": "Doe"
+	},
+	{
+	"ID": 2,
+	"firstName": "Jane",
+	"lastName": "Smith"
+	},
+	{
+	"ID": 3,
+	"firstName": "Michael",
+	"lastName": "Johnson"
+	},
+	{
+	"ID": 4,
+	"firstName": "Emily",
+	"lastName": "Davis"
+	},
+	{
+	"ID": 5,
+	"firstName": "Chris",
+	"lastName": "Brown"
+	}
+]
+
+//GET METHODS
+router.get("/users", (req, res) => {
+	res.json(usersArray)
+})
+
+//POST METHODS
+app.post("/users", (req, res) => {
+	const { firstName, lastName } = req.body
+
+	res.json({
+		msg: "This the message from PUT ",
+		firstName,
+		lastName,
+	})
+})
+
+//PUT METHODS
+app.put("/users", (req, res) => {
+	const { firstName, lastName } = req.body
+  const userId = req.params.id
+
+	res.json({
+		msg: "This the message from PUT ",
+    userId,
+		firstName,
+		lastName,
+	})
+})
+
+//DELETE METHODS
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+	res.json({
+		msg: "This the message from DELETE ",
+    id
+	})
+})
+
+module.exports = router
